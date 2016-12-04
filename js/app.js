f59c66f3d139d570e49f857161ed17ae229cc15c
KonradSchieban <konrad.schieban@gmail.com>
2016-12-04 15:36:46 -0500

    Wikipedia Search works

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var map;
-var infowindow;  
-	  
+var infowindow;
+var markers = [];  
+	
 function initMap(positionInput, radiusInput, nearbySearchTypeInput) {
 	
 	if(!positionInput){
@@ -20,18 +21,22 @@ function initMap(positionInput, radiusInput, nearbySearchTypeInput) {
 
 	infowindow = new google.maps.InfoWindow();
 	var service = new google.maps.places.PlacesService(map);
-	service.nearbySearch({
+	var a = service.nearbySearch({
 		location: positionInput,
 		radius: radiusInput,
 		type: [nearbySearchTypeInput]
 	}, callback);
-	console.log(nearbySearchTypeInput);
+	
+	console.log(a);
+	
 }
 
 function callback(results, status) {
+	markers = [];
 	if (status === google.maps.places.PlacesServiceStatus.OK) {
 		for (var i = 0; i < results.length; i++) {
 			createMarker(results[i]);
+			markers.push(results[i]);
 		}
 	}
 }
@@ -67,6 +72,77 @@ var Model = function(data){
 	this.coordinates = ko.observable(data.coordinates);	
 	this.searchString = ko.observable(data.searchString);
 	this.searchRadius = ko.observable(data.searchRadius);
+	this.markersMap = ko.observableArray(data.markersMap);
+	this.currentItem = ko.observable(data.currentItem);
+	this.errorMessage = ko.observable(data.errorMessage);
+	
+	this.wikiResponse = ko.observable();
+	ko.computed(function() {
+		
+		console.log("computing wikiResponse");
+		// load wikipedia data
+		
+		if(this.currentItem()){
+			
+			console.log("actually computing wikiResponse");
+			
+			var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + this.currentItem() + '&format=json&callback=wikiCallback';
+		
+			// Whenever "pageIndex", "sortColumn", or "sortDirection" change, this function will re-run and issue
+			// an Ajax request. When the Ajax request completes, assign the resulting value to "queryResults"
+			$.ajax({
+				url: wikiUrl,
+				dataType: "jsonp",
+				jsonp: "callback",
+				success: this.wikiResponse
+			});
+			
+		}else{
+			console.log("not actually computing wikiResponse");
+		}
+		
+	}, this);
+
+	
+	this.wikiJSON = ko.computed(function(){
+		
+		var wikiJSONnew = [];
+		
+		console.log("computing wikiJSON");
+		
+		if(this.wikiResponse()){
+			
+			console.log("actually computing wikiJSON");
+			console.log(this.wikiResponse);
+			console.log(this.wikiResponse());
+		
+			var nameList = this.wikiResponse()[1];
+			var descriptionList = this.wikiResponse()[2];
+			var urlList = this.wikiResponse()[3];
+			
+			for (var i = 0; i < nameList.length; i++) {
+				
+				var nameStr = nameList[i];
+				var descriptionStr = descriptionList[i];
+				var urlStr = urlList[i];
+				
+				var wikiJSONpart = {
+					"name":nameStr,
+					"description":descriptionStr,
+					"url":urlStr
+				}
+				
+				wikiJSONnew.push(wikiJSONpart);
+			};
+		}else{
+			console.log("not actually computing wikiJSON");
+		}
+		
+		console.log("wikiJSONnew:");
+		console.log(wikiJSONnew);
+		
+		return wikiJSONnew;
+	},this);
 	
 }
 
@@ -76,34 +152,59 @@ var ViewModel = function(){
 	this.model = ko.observable(new Model({
 		coordinates: {lat: 42.33800859999999, lng: -71.1251311},
 		searchString: "Cafe",
-		searchRadius: 100
+		searchRadius: 100,
+		markersMap: markers,
+		currentItem: "",
+		errorMessage: ""
 	})) ;
 	
 	this.search = function() {
 		
 		var inputSearchStrRaw = $('#searchLocation').val();
-		inputSearchStrRaw.replace(/ /g, "+");
-		this.searchString(inputSearchStrRaw);
-		
 		var inputRadiusStrRaw = $('#searchRadius').val();
-		this.searchRadius(inputRadiusStrRaw);
-		
 		var inputStringStrRaw = $('#searchString').val();
-		this.searchString(inputStringStrRaw);
 		
-		var mapsGetResponse = $.ajax({
-			url: 'http://maps.google.com/maps/api/geocode/json?address=' + inputSearchStrRaw,
-			success: function (result) {
-				if (result.isOk == false) alert(result.message);
-			},
-			async: false
-		});
+		this.errorMessage("");
 		
-		this.coordinates(mapsGetResponse.responseJSON.results[0].geometry.location);
+		if(inputSearchStrRaw && inputRadiusStrRaw && inputStringStrRaw){
 		
-		initMap(this.coordinates(), this.searchRadius(), this.searchString());
+			inputSearchStrRaw.replace(/ /g, "+");
+			this.searchString(inputSearchStrRaw);
+			this.searchRadius(inputRadiusStrRaw);
+			this.searchString(inputStringStrRaw);
+			
+			var mapsGetResponse = $.ajax({
+				url: 'http://maps.google.com/maps/api/geocode/json?address=' + inputSearchStrRaw,
+				success: function (result) {
+					if (result.isOk == false) alert(result.message);
+				},
+				async: false
+			});
+			
+			if(mapsGetResponse.responseJSON.status == "OK"){
+				
+				//if Google Maps finds something, update coordinates and markers observables
+				this.coordinates(mapsGetResponse.responseJSON.results[0].geometry.location);
+				this.markersMap(markers);
+				
+				//reload map...
+				initMap(this.coordinates(), this.searchRadius(), this.searchString());
+				
+			}else{
+				this.errorMessage("Did not find a Google Maps Location");
+			}
+		}else{
+			this.errorMessage("Please fill in all input forms!");
+		}	
 		
-    }
+    };
+	
+	// this function is executed when somebody clicks on a list item next to the map
+	this.setCurrentItem = function(searchStr) {
+
+		this.model().currentItem(searchStr);
+		
+	};
 	
 }
 
